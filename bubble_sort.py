b9dbcc74627da50e44b7dcabec4d02958abc7592
malobaidli26 <ma26949@essex.ac.uk>
2026-02-25 11:04:37 +0300

    Add bubble sort implementation

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+numbers = [5, 2, 9, 1, 5, 6]
+sorted_numbers = bubble_sort(numbers)
+
+print("Sorted list:", sorted_numbers)
